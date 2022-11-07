5d0731427c7bde5003e8bf80f2c77253629e95a6
Kurt Hu <kurthu2@gmail.com>
2022-11-07 15:20:11 -0500

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::io::Write;
+#[allow(unused_imports)]
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +13,24 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
-        Err(e) => println!("couldn't accept client: {:?}", e),
+    // match listener.accept() {
+    //     Ok((stream, addr)) => {
+    //         handle_accept_client(stream, addr);
+    //     },
+    //     Err(e) => println!("couldn't accept client: {:?}", e),
+    // }
+    for stream in listener.incoming() {
+        let stream: TcpStream = stream.unwrap();
+        handle_connection(stream);
     }
+}
+
+// fn handle_accept_client(stream:TcpStream, addr: std::net::SocketAddr) {
+//     println!("accepted new client: {:?}", addr);
+// }
+
+fn handle_connection(mut stream: TcpStream) {
+    let response: &str = "+PONG\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
